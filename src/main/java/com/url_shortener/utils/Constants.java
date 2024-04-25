a7e1df3f89104c8a170526c0227e8a7e35155753
Ivan <learpcs@gmail.com>
2024-04-25 22:30:12 +0400

    Link size is changed to 5

---
+++ src/main/java/com/url_shortener/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.url_shortener.utils;
+
+public class Constants {
+    public static final Integer ID_SIZE = 5;
+    public static final Long ID_UPPER_BOUND = 916132832L; //62 ** 5
+    public static final Integer URLSIZE_UPPER_BOUND = 2048;
+}
--- src/main/java/com/url_shortener/utils/ConstantsClass.java
+++
@@ -1,7 +0,0 @@
-package com.url_shortener.utils;
-
-public class ConstantsClass {
-    public static final Integer ID_SIZE = 8;
-    public static final Long ID_UPPER_BOUND = 218340105584896L; //62 ** 8
-    public static final Integer URLSIZE_UPPER_BOUND = 2048;
-}
