157d9ac2b867041efc737def4b401ba7758fbbbf
Ivan <learpcs@gmail.com>
2024-04-25 22:33:08 +0400

    Made redirect controller simpler

--- src/main/java/com/url_shortener/controller/RedirectController.java
+++ src/main/java/com/url_shortener/controller/RedirectController.java
@@ -1,24 +1,32 @@
 package com.url_shortener.controller;
 
 import com.url_shortener.exception.ConverterException;
+import com.url_shortener.exception.UrlNotFoundException;
+import com.url_shortener.repository.dao.UrlDao;
 import com.url_shortener.service.UrlService;
-import com.url_shortener.utils.IdUrlConverter;
-import jakarta.servlet.http.HttpServletRequest;
+import com.url_shortener.utils.Mappers.IdUrlMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Optional;
+
+//TODO dynamic mapping
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/redirect")
 public class RedirectController {
     @Autowired
     UrlService urlService;
 
-    //To-do dynamic mapping
-    @GetMapping("ecaaaaaa")
-    public RedirectView redirect() throws ConverterException {
-        return new RedirectView(urlService.findById(IdUrlConverter.getId("ecaaaaaa".toCharArray())).get().getUrl());
+    @GetMapping
+    public RedirectView redirect(@RequestParam String url) throws ConverterException, UrlNotFoundException {
+
+        return new RedirectView(
+                urlService.findById(IdUrlMapper.getId(url)).orElseThrow(() -> new UrlNotFoundException("Short url doesn't exist")).getUrl()
+        );
     }
 }
